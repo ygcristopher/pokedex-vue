875e6476b26eea46a2a757231e6c305b6aecd5ff
Yago Cristopher <yago1900@gmail.com>
2023-12-02 12:39:14 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,41 @@
+<script>
+import axios from 'axios';
+import { RouterView } from 'vue-router'
+import Pokemon from './components/Pokemon.vue'
+
+  export default {
+    name: 'App',
+    data() {
+     return {
+      pokemons: []
+    }
+  },
+    created: function() {
+      axios.get('https://pokeapi.co/api/v2/pokemon?limit=151&offset=0')
+           .then(res => {
+            console.log('Lista de pokemons')
+            this.pokemons = res.data.results
+           })
+  },
+  components: { 
+    Pokemon
+  }}
+
+</script>
+
+<template>
+  <header>
+
+    <div class="app">
+      <div v-for="(poke,index) in pokemons" :key="index"> 
+         <Pokemon :name="poke.name" :url="poke.url" :num="index + 1"/>
+      </div>
+    </div>
+  </header>
+
+  <RouterView />
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/Pokemon.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <h1> {{ num }}  {{ upper }} </h1>
+        <small>{{ url }}</small>
+    </div>
+</template>
+
+<script>
+  import axios from 'axios';
+    export default {
+        props: {
+            num: Number,
+            name: String,
+            url: String
+        },
+        computed: {
+            upper: function(){
+                var newName = this.name[0].toUpperCase() + this.name.slice(1);
+             return newName;
+        },
+}}
+</script>
+
+<style>
+
+</style>
